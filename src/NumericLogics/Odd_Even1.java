581c541fd07909a53924771a94fae1c74e61f56b
Ajaykumar Vijayakumar <ajay.k@ycs.in>
2025-04-12 19:49:47 +0530

    Files Added

---
+++ src/NumericLogics/Odd_Even1.java
@@ -0,0 +1,21 @@
+package NumericLogics;
+
+import java.util.Scanner;
+
+public class Odd_Even1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number : ");
+        System.out.println(isEven(sc.nextInt()));
+        System.out.println(isOdd(sc.nextInt()));
+    }
+
+    public static boolean isOdd(int num) {
+        return (num / 2) * 2 != num;
+    }
+
+    public static boolean isEven(int num) {
+        return (num / 2) * 2 == num;
+    }
+}
