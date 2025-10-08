a23495d161e97bd6f2bcfa11d5b41ebffd38854e
Ajaykumar Vijayakumar <ajay.k@ycs.in>
2025-10-08 08:10:42 +0530

    Binary search

---
+++ src/BinarySearch/BinarySearch.java
@@ -0,0 +1,29 @@
+package BinarySearch;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 45, 65, 6, 878, 9, 78};
+
+        System.out.println("Index " + binarySearch(arr, 878));
+    }
+
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > target){
+                end = mid - 1;
+            }
+            else if(arr[mid] < target){
+                start = mid + 1;
+            }
+            else{
+                return  mid;
+            }
+        }
+
+        return -1 ;
+    }
+}
